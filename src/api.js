b45c5c97611e5c95a3ec41ce5df177b43334eb9f
ESM <111918644+HoneATCS@users.noreply.github.com>
2023-02-23 18:48:56 +0200

    cor

---
+++ src/api.js
@@ -0,0 +1,9 @@
+
+var API_KEY = '33774181-bed99a30315d4582da4b976c0';
+var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent(' ');
+// $.getJSON(URL, fetch(data){
+// if (parseInt(data.totalHits) > 0)
+//     $.each(data.hits, fun(i, hit){ console.log(hit.pageURL); });
+// else
+//     console.log('No hits');
+// });
